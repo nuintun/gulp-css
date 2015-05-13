41ed9425ea5d3ff47f356fdc139a09b135d79b36
Nuintun <nuintun@qq.com>
2015-05-13 11:18:07 +0800

    add source files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# gulp-css
-A gulp plugin for css transport and concat
+gulp-css
+=========
+
+>A gulp plugin for css transport and concat
+
+>[![Dependencies][david-image]][david-url]
+
+[david-image]: http://img.shields.io/david/nuintun/gulp-css.svg?style=flat-square
+[david-url]: https://david-dm.org/Nuintun/gulp-css
--- index.js
+++ index.js
@@ -3,4 +3,45 @@
  */
 
 'use strict';
- 
+
+var through = require('through2');
+var duplexer = require('duplexer2');
+var util = require('./lib/util');
+var rename = require('./lib/rename');
+var cache = require('./lib/cache');
+var common = require('./lib/common');
+var include = require('./lib/include');
+var concat = require('./lib/concat');
+
+function main(options){
+  var input = through.obj({ objectMode: true });
+  var output = through.obj({ objectMode: true });
+
+  var monitor = function (stream){
+    stream.on('error', function (e){
+      output.emit('error', e);
+    });
+
+    return stream;
+  };
+
+  monitor(input)
+    .pipe(monitor(include(options)))
+    .pipe(monitor(concat()))
+    .pipe(output);
+
+  return duplexer(input, output);
+}
+
+/**
+ * Exports module.
+ */
+
+main.cache = {};
+main.cache.clean = cache.clean;
+main.cwd = util.cwd;
+main.debug = util.debug;
+main.colors = util.colors;
+main.createParser = common.createParser;
+
+module.exports = main;
