9b59cb2b6eea0fbfec01ef5d8abc28fd8962aaf3
nuintun <nuintun@qq.com>
2020-06-17 17:10:46 +0800

    :tada: Update files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @module @nuintun/gulp-css
  * @author nuintun
  * @license MIT
- * @version 1.0.0
+ * @version 2.0.0
  * @description A gulp plugin for cmd transport and concat.
  * @see https://github.com/nuintun/gulp-css#readme
  */
